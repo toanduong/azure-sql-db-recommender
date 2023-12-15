43f95cc7db38acc201e52ee77fb0531665d0c624
toanduong <duongmanhtoan86@gmail.com>
2023-12-15 20:37:54 +0700

    add source

---
+++ client/src/routes/sessionsList.jsx
@@ -0,0 +1,75 @@
+import { useEffect } from "react";
+import { Form, useLoaderData, useNavigation, } from "react-router-dom";
+import { getSessions } from "../sessions";
+
+export async function loader({ request }) {
+    const url = new URL(request.url);
+    const searchQuery = url.searchParams.get("q") ?? "";
+    var isSearch = (searchQuery == "") ? false : true;
+    var { sessions, errorInfo } = isSearch ? await getSessions(searchQuery) : {sessions:[], errorInfo:null};
+    if (!Array.isArray(sessions)) { 
+        errorInfo = { errorMessage: "Error: sessions is not an array" };
+        sessions = [];
+    }
+    return { sessions, searchQuery, isSearch, errorInfo };
+}
+
+export default function SessionsList() {
+    const { sessions, searchQuery, isSearch, errorInfo } = useLoaderData();
+    const navigation = useNavigation();
+
+    const searching =
+        navigation.location &&
+        new URLSearchParams(navigation.location.search).has(
+            "q"
+        );
+
+    useEffect(() => {
+        document.getElementById("q").value = searchQuery;
+    }, [searchQuery]);
+
+    return (
+        <>
+            <div id="searchbox">
+                <div>
+                    <Form id="search-form" role="search">
+                        <input
+                            id="q"
+                            className={searching ? "loading" : ""}
+                            aria-label="Search sessions"
+                            placeholder="Search"
+                            type="search"
+                            name="q"
+                            defaultValue={searchQuery}
+                        />
+                        <div id="search-spinner" aria-hidden hidden={!searching} />
+                        <div className="sr-only" aria-live="polite"></div>
+                        <button type="submit">Search</button>
+                    </Form>
+                </div>
+            </div>
+            <div id="sessions" className={navigation.state === "loading" ? "loading" : ""}>
+                {!errorInfo ? "" : <p id="error"><i>{"Error" + errorInfo.errorMessage}</i></p>}
+                {sessions.length ? (
+                    <section className="agenda-group">
+                        {sessions.map((session) => (
+                            <div>
+                                <p className="font-title-2"><a href={"#" + session.id}>{session.title}</a></p>
+                                <p className="session-similarity font-stronger">Similarity: {session.cosine_similarity.toFixed(6)}</p>
+                                <p className="font-subtitle-2 abstract">
+                                    {session.abstract}
+                                </p>
+                            </div>
+                        ))}
+                    </section>
+                ) : (
+                    <p>
+                        <i><center>
+                            { isSearch ? "No session found" : "" }
+                        </center></i>
+                    </p>
+                )}
+            </div>
+        </>
+    );
+}
---
+++ func/SessionProcessor.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.WebJobs.Extensions.Sql;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Net;
+using Dapper;
+
+namespace SessionRecommender.SessionProcessor
+{
+    public class Session
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Abstract { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Session)
+            {
+                var that = obj as Session;
+                return this.Id == that.Id && this.Title == that.Title && this.Abstract == that.Abstract;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode() ^ this.Title.GetHashCode() ^ this.Abstract.GetHashCode();
+        }
+    }
+
+    public static class SessionProcessor
+    {
+        private static HttpClient httpClient;
+
+        static SessionProcessor()
+        {
+            var key = Environment.GetEnvironmentVariable("AzureOpenAI.Key");
+            var endpoint = Environment.GetEnvironmentVariable("AzureOpenAI.Endpoint");
+
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(endpoint);
+            httpClient.DefaultRequestHeaders.Add("api-key", key);
+        }
+
+        [FunctionName("SessionProcessor")]
+        public static async Task Run(
+            [SqlTrigger("[web].[sessions]", "AzureSQL.ConnectionString")]
+            IReadOnlyList<SqlChange<Session>> changes,
+            ILogger logger)
+        {
+            logger.LogInformation("Detected: " + changes.Count + " change(s).");
+
+            foreach (var change in changes)
+            {
+                if (change.Operation == SqlChangeOperation.Delete) continue;
+                logger.LogInformation($"[{change.Item.Id}] Processing change for operation: " + change.Operation.ToString());
+
+                var attempts = 0;
+                var embeddingsReceived = false;
+                while (attempts < 3)
+                {
+                    attempts++;
+
+                    logger.LogInformation($"[{change.Item.Id}] Attempt {attempts} of 3 to get embeddings.");
+
+                    var deploymentName = Environment.GetEnvironmentVariable("AzureOpenAI.DeploymentName");
+                    var requestUri = "/openai/deployments/" + deploymentName + "/embeddings?api-version=2023-03-15-preview";
+                    var response = await httpClient.PostAsJsonAsync(
+                        requestUri,
+                        new { input = change.Item.Title + ':' + change.Item.Abstract }
+                    );
+
+                    if (response.StatusCode == HttpStatusCode.TooManyRequests)
+                    {
+                        var waitFor = response.Headers.RetryAfter.Delta.Value.TotalSeconds;
+                        logger.LogInformation($"[{change.Item.Id}] OpenAI had too many requests. Waiting {waitFor} seconds.");                        
+                        await Task.Delay(TimeSpan.FromSeconds(waitFor));
+                        continue;
+                    }
+
+                    response.EnsureSuccessStatusCode();
+
+                    var jd = await response.Content.ReadAsAsync<JObject>();
+                    var e = jd.SelectToken("data[0].embedding");
+                    if (e != null)
+                    {
+                        using var conn = new SqlConnection(Environment.GetEnvironmentVariable("AzureSQL.ConnectionString"));
+                        await conn.ExecuteAsync(
+                            "web.upsert_session_abstract_embeddings",
+                            commandType: CommandType.StoredProcedure,
+                            param: new
+                            {
+                                @session_id = change.Item.Id,
+                                @embeddings = e.ToString()
+                            });                           
+                        embeddingsReceived = true;                            
+                        logger.LogInformation($"[{change.Item.Id}] Done.");
+                    } else {
+                        logger.LogInformation($"[{change.Item.Id}] No embeddings received.");
+                    }
+                    
+                    break;                 
+                }
+                if (!embeddingsReceived) {
+                    logger.LogInformation($"[{change.Item.Id}] Failed to get embeddings.");
+                }
+            }
+        }
+    }
+}
